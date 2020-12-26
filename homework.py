ad754ac31a6d8e101cc12af11b2b3a6cc9214db2
Marina <marinella2012@gmail.com>
2020-12-26 20:30:09 +0400

    My first commit

--- homework.py
+++ homework.py
@@ -1,24 +1,42 @@
+import os
 import time
-import requests
 
+import requests
+from dotenv import load_dotenv
 from twilio.rest import Client
+
+load_dotenv()
 
 
 def get_status(user_id):
+    vk_token = os.getenv('VK_AUTH_TOKEN')
     params = {
-        ...
+        'v': '5.89',
+        'user_ids': user_id,
+        'access_token': vk_token,
+        'fields': 'online'
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
+    get_account = requests.post(
+        'https://api.vk.com/method/users.get',
+        params=params
+        )
+    status = get_account.json().get('response')[0].get('online')
+    return status
 
 
-def send_sms(sms_text, client):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+def send_sms(sms_text):
+    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body=sms_text,
+        from_=os.getenv('NUMBER_FROM'),
+        to=os.getenv('NUMBER_TO')
+    )
+    return message.sid
 
 
 if __name__ == '__main__':
-    # тут происходит инициализация Client
     vk_id = input('Введите id ')
     while True:
         if get_status(vk_id) == 1:
